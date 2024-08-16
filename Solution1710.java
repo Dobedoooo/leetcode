3d4b358fceda6fedf9e6bcf78469df2ac726d85b
dobedoo <1114170770@qq.com>
2024-08-16 08:38:45 +0800

    optm 2720

--- Solution1710.java
+++ Solution1710.java
@@ -16,9 +16,28 @@ public class Solution1710 {
         }
         return maxUnits;
     }
+
+    public int maximumUnitsOptm(int[][] boxTypes, int truckSize) {
+        // 由于 boxTypes.length <= 1000，使用计数排序优化排序速度
+        int[] type = new int[1001];
+        for(int[] boxType: boxTypes) type[boxType[1]] += boxType[0];
+        int maxUnits = 0;
+        for(int i = type.length - 1; i >= 0; i--) {
+            if(type[i] == 0) continue;
+            if(truckSize >= type[i]) {
+                maxUnits += i * type[i];
+                truckSize -= type[i];
+            } else {
+                maxUnits += truckSize * i;
+                return maxUnits;
+            }
+        }
+        return maxUnits;
+    }
+
     public static void main(String[] args) {
         int[][] boxTypes = { { 1, 3 }, { 2, 2 }, { 3, 1 } };
         int trackSize = 4;
-        System.out.println(new Solution1710().maximumUnits(boxTypes, trackSize));
+        System.out.println(new Solution1710().maximumUnitsOptm(boxTypes, trackSize));
     }
 }
